d546174bb80b9a15a9d1e324b8816c7b93dfd2c8
Billy Arredondo <22175195+gubiarpa@users.noreply.github.com>
2019-04-15 18:45:47 -0500

    CustomerController: Se agregó la acción Detail.

--- wawishapp/Controllers/CustomersController.cs
+++ wawishapp/Controllers/CustomersController.cs
@@ -22,5 +22,19 @@ namespace wawishapp.Controllers
         {
             return View();
         }
+
+        public ActionResult Details(int Id)
+        {
+            if (Id > 0)
+            {
+                var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == Id);
+
+                if (customer == null)
+                    return new EmptyResult();
+
+                return View(customer);
+            }
+            return new HttpNotFoundResult();
+        }
     }
 }
